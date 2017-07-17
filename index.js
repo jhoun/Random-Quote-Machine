e6738161bd5a5f5d40e7a5d286215926b862dd8c
jhoun <jayhoun@gmail.com>
2017-07-17 15:09:14 -0700

    added tweet button

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-$("button").click(function(){
 
-  $.ajax({url:"http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", cache:false, success:function(result){
-    $("#message").html(result[0].content + "- " + result[0].title);
+$("#getQuote").click(function(){
+
+  $.ajax({
+    url:"http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+    cache:false,
+    success:function(result){
+      var quote = result[0].content;
+      var author = result[0].title;
+      $("#message").html( quote + "- " + author );
+      $("#tweet").click(function(){
+        var twtLink = `http://twitter.com/home?status=${encodeURIComponent($(quote).text() + " - " + author)}`;
+        window.open(twtLink,'_blank');
+      })
   }});
 })
